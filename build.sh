cbd94689f2d9dfacf61ad7c7077af8f0e6e2663a
David Lawson <david_lawson@dell.com>
2022-09-16 13:14:08 -0500

    Initial commit

---
+++ build.sh
@@ -0,0 +1,37 @@
+
+function error_exit() {
+    echo "$*"
+    exit 99
+}
+
+function on_exit() {
+    cd "${topdir}"
+}
+
+topdir="$(dirname "$(realpath "${0}")")"
+
+set -e
+
+trap on_exit EXIT
+
+echo "****************************************************************************"
+echo "topdir=${topdir}"
+echo "****************************************************************************"
+ext_build_name="package1"
+config_name="${ext_build_name}-release_defconfig"
+dot_config_name=."${config_name}"
+br2_out_dir="${topdir}/${ext_build_name}-release-output"
+brdir="${topdir}/buildroot"
+br2_externals="${topdir}/pkg1"
+
+
+pkgdir="${topdir}/pkg1"
+cd "${pkgdir}" || error_exit "could not change into ${pkgdir}"
+if [ -e "configs"/${dot_config_name} ] ; then
+    echo "already have defconfig: ${sdkdir}/configs/${dot_config_name}"
+else 
+    cp "${brdir}/.config" "configs/${dot_config_name}"
+fi
+
+mkdir -p "${br2_out_dir}" || error_exit "Could not create output directory ${br2_out_dir}"
+make -d -C "${brdir}" O="${br2_out_dir}" BR2_EXTERNAL="${br2_externals}" "${dot_config_name}"
---
+++ pkg1/external.mk
@@ -0,0 +1,1 @@
+include $(wildcard $(BR2_EXTERNAL_TEST_PACKAGE_1_PATH)/package/*/*.mk)
